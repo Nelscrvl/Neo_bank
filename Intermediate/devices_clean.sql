a32e37a6403c061ed388f47ec6ab534a194b81bf
chronique_aere.0z@icloud.com <chronique_aere.0z@icloud.com>
2023-12-04 15:11:32 +0000

    - Delete row with aberrant value

--- Intermediate/devices_clean.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-string_field_0 AS devices, 
-string_field_1 AS user_id
-FROM {{ ref('stg_raw__devices') }}
-
---
+++ models/Intermediate/devices_clean.sql
@@ -0,0 +1,16 @@
+WITH renamed AS (SELECT 
+    string_field_0 AS device, 
+    string_field_1 AS user_id
+  FROM {{ ref('stg_raw__devices') }}
+
+)
+
+SELECT *
+FROM renamed
+WHERE device != 'brand'
+
+
+
+
+
+
