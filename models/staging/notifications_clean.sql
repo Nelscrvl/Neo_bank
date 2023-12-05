71f003217e9c15e5443c5e9644f8e5245ca88989
Julia Bornert <jul.bornert@gmail.com>
2023-12-05 14:49:25 +0000

    date column restitution

--- models/staging/notifications_clean.sql
+++ models/staging/notifications_clean.sql
@@ -4,9 +4,6 @@ reason,
 channel,
 status,
 user_id,
-EXTRACT(year FROM created_date) AS creation_year,
-EXTRACT(month FROM created_date) AS creation_month,
-EXTRACT(day FROM created_date) AS creation_day,
-EXTRACT(hour FROM created_date) AS creation_hour
+DATE (created_date) AS created_date
 
 FROM {{ ref('stg_raw__notifications') }}
