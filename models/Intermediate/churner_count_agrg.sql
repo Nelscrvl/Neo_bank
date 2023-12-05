8bb88548725619b5f4ceec0d026859d248181015
chronique_aere.0z@icloud.com <chronique_aere.0z@icloud.com>
2023-12-05 14:09:02 +0000

    - Group by user_id / nb tx by user and sum_tx by user and turnover by user

--- models/Intermediate/churner_count_agrg.sql
+++ models/Intermediate/churner_count_agrg.sql
@@ -2,7 +2,9 @@ SELECT
 
 user_id,
 COUNT (transaction_id) AS nb_transaction,
-AVG (amount_usd) AS avg_amout
+ROUND(SUM (amount_usd)) AS sum_transaction_usd,
+ROUND(SUM(CAST(amount_usd * 0.0025 AS FLOAT64))) AS turnover_by_user
 
 FROM{{ ref('transaction_clean') }}
 GROUP BY user_id
+
